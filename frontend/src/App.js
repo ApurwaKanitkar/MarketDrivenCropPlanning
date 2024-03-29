37249be4b5ebad51f3c2d7d332f22285d5b8467d
Aditya Nidhonkar <adityanidhonkar2004@gmail.com>
2024-03-29 15:30:41 +0530

    navbar ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
 
 const App = () => {
-  const ref = useRef(null);
-  console.log(ref);
   return (
     <div>
-      <h2 className=" text-3xl">Rice Area Over Time in Gwalior</h2>
-      <input
-        ref={ref}
-        type="text"
-        name="inputField"
-        id="inputField"
-        className="border border-black mt-[40px] "
-      ></input>
-      <img src="http://localhost:5000/plot" alt="Plot" />
+      <Navbar />
+      <Filter />
     </div>
   );
 };
