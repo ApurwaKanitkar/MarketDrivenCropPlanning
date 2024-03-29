57c80e70c6cfb45afa2062a79e394a7ce84d2578
Hrishikesh Patkar <patkarhrishikesh0204@gmail.com>
2024-03-29 11:52:27 +0530

    commit1

--- data-analytics/plot.py
+++ data-analytics/plot.py
@@ -2,9 +2,9 @@ import matplotlib
 matplotlib.use('Agg')  # Required for non-GUI backend
 import matplotlib.pyplot as plt
 
-def create_plot1(data):
+def create_plot1(data1):
     # graph1
-    Gwalior_df = data
+    Gwalior_df = data1
     plt.figure(figsize=(10, 4))
     plt.plot(Gwalior_df['Year'], Gwalior_df['RICE AREA (1000 ha)'], marker='o', linestyle='-', color='b')
     plt.title('Rice Area Over Time in Gwalior')
@@ -14,7 +14,7 @@ def create_plot1(data):
 
     return plt
 
-def create_plot2(data):
+def create_plot2(data2):
 
     #graph2
     plt.figure(figsize=(10, 6))
